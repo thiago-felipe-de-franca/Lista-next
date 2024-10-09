fd2fa2c9c2490c0ce838a44f8f4687c073bdb578
thiago-felipe-franca <thiagofelipe_franca@gmail.com>
2024-10-09 11:14:55 -0300

    Feat: Criação a feature Quadrado

---
+++ src/app/components/quadrado.tsx
@@ -0,0 +1,12 @@
+export function Quadrado() {
+    return (
+    <>
+        <div style = {{
+            backgroundColor: 'Blue',
+            width: '100px',
+            height: '100px'
+        }}>
+        </div>
+    </>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Quadrado } from "./components/quadrado";
+
+export default function Home() {
+  return (
+    <>
+    <Quadrado/>
+    </>
+  )
+}
